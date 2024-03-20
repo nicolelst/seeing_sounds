e29831e9964b41dab72ed615fd2cfff1ceefb70e
nicolelst <nicolelim2608@gmail.com>
2024-03-21 01:26:31 +0800

    settings: add accordion and make scrollable

--- application/fe/src/components/stages/settingsStage.tsx
+++ application/fe/src/components/stages/settingsStage.tsx
@@ -9,9 +9,16 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/shadcn/components/ui/select";
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/shadcn/components/ui/accordion";
 import { FormInputs } from "@/types/formInputs";
 import { ReactElement, ReactNode } from "react";
 import { UseFormRegister, FieldError, FieldErrors } from "react-hook-form";
+import { ScrollArea } from "@/shadcn/components/ui/scroll-area";
 
 interface SettingsStageProps {
 	register: UseFormRegister<FormInputs>;
@@ -24,62 +31,136 @@ export default function SettingsStage({
 }: SettingsStageProps): ReactElement {
 	return (
 		<div className="flex flex-col w-full h-full">
-			<div className="flex flex-col w-full h-full">
-				{/* <input {...register("exampleRequired", { required: true })} />
+			{/* <input {...register("exampleRequired", { required: true })} />
 				{errors.exampleRequired && <span>This field is required</span>} */}
-				<div className="grid gap-4 py-4 w-full max-w-2xl mx-auto">
-					<SettingItem
-						label="How many speakers are in the video?"
-						error={errors.numSpeakers}
-					>
-						<Input
-							{...register("numSpeakers", {
-								required: true,
-								valueAsNumber: true,
-								min: 1,
-								// TODO reject decimals, letters
-								// setValueAs: v => parseInt(v),
-								// onChange: (e) => {
-								//   return e.target.value.replace(/[^1-9]/g, '');
-								// },
-							})}
-						/>
-					</SettingItem>
-					<SettingItem
-						label="Select captioning interface"
-						error={undefined}
-					>
-						{/* TODO info button for interface preview/explanation */}
-						{/* TODO register annotationType */}
-						{/* TODO enable when options available */}
-						<Select disabled defaultValue="floating">
-							<SelectTrigger>
-								<SelectValue placeholder="Select an interface" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectGroup>
-									<SelectItem value="floating">
-										Floating captions (default)
-									</SelectItem>
-									<SelectItem value="colour">
-										Static captions coloured by speaker
-									</SelectItem>
-									<SelectItem value="pointer">
-										Static captions with pointers
-									</SelectItem>
-									<SelectItem value="traditional">
-										Traditional static captions
-									</SelectItem>
-								</SelectGroup>
-							</SelectContent>
-						</Select>
-					</SettingItem>
-				</div>
-			</div>
+			<Accordion
+				type="single"
+				collapsible
+				defaultValue="video"
+				className="w-full h-full max-w-2xl mx-auto"
+			>
+				<SettingAccordionItem value="video" header="Video annotation">
+					<div className="flex flex-col gap-4">
+						<SettingItem
+							label="How many speakers are in the video?"
+							error={errors.numSpeakers}
+						>
+							<Input
+								{...register("numSpeakers", {
+									required: true,
+									valueAsNumber: true,
+									min: 1,
+									// TODO reject decimals, letters
+									// setValueAs: v => parseInt(v),
+									// onChange: (e) => {
+									//   return e.target.value.replace(/[^1-9]/g, '');
+									// },
+								})}
+							/>
+						</SettingItem>
+						<SettingItem
+							label="Select captioning interface"
+							error={undefined}
+						>
+							{/* TODO info button for interface preview/explanation */}
+							{/* TODO register annotationType */}
+							{/* TODO enable when options available */}
+							{/* TODO change value based on form */}
+							<InterfaceSelector value="floating" />
+						</SettingItem>
+					</div>
+				</SettingAccordionItem>
+				<SettingAccordionItem
+					value="transcript"
+					header="Video transcript"
+				>
+					<div className="flex flex-col gap-4">
+						<SettingItem
+							label="Setting"
+							error={undefined}
+							// errors.numSpeakers
+						>
+							<Input
+							// {...register("numSpeakers", {
+							// 	required: true,
+							// })}
+							/>
+						</SettingItem>
+					</div>
+				</SettingAccordionItem>
+				<SettingAccordionItem
+					value="speechSep"
+					header="Speech separation model parameters"
+				>
+					<div className="flex flex-col gap-4">
+						<SettingItem
+							label="Setting"
+							error={undefined}
+							// errors.numSpeakers
+						>
+							<Input
+							// {...register("numSpeakers", {
+							// 	required: true,
+							// })}
+							/>
+						</SettingItem>
+					</div>
+				</SettingAccordionItem>
+				<SettingAccordionItem
+					value="speechRec"
+					header="Speech recognition model parameters"
+				>
+					<div className="flex flex-col gap-4">
+						<SettingItem
+							label="Setting"
+							error={undefined}
+							// errors.numSpeakers
+						>
+							<Input
+							// {...register("numSpeakers", {
+							// 	required: true,
+							// })}
+							/>
+						</SettingItem>
+					</div>
+				</SettingAccordionItem>
+			</Accordion>
 			<Button className="w-fit my-2 self-end" type="submit">
 				Next
 			</Button>
 		</div>
+	);
+}
+
+interface SettingAccordionItemProps {
+	value: string;
+	header: string;
+	children: ReactNode;
+	scrollable?: boolean;
+}
+
+function SettingAccordionItem({
+	value,
+	header,
+	children,
+	scrollable,
+}: SettingAccordionItemProps): ReactElement {
+	return (
+		<AccordionItem value={value}>
+			<AccordionTrigger className="text-lg font-semibold hover:no-underline py-3">
+				{header}
+			</AccordionTrigger>
+			<AccordionContent className="px-1 pt-1 pb-4">
+				{scrollable ?? false ? (
+					<ScrollArea className="h-52 pr-4">
+						{/* TODO dynamically determine size based on screen */}
+						{children}
+					</ScrollArea>
+				) : (
+					children
+				)}
+			</AccordionContent>
+		</AccordionItem>
 	);
 }
 
@@ -103,5 +184,38 @@ function SettingItem({
 				<p className="text-red-600">{error?.message}</p>
 			</div>
 		</div>
+	);
+}
+
+interface InterfaceSelectorProps {
+	disabled?: boolean;
+	value?: string;
+}
+function InterfaceSelector({
+	disabled,
+	value,
+}: InterfaceSelectorProps): ReactElement {
+	return (
+		<Select disabled={disabled ?? false} defaultValue={value ?? "floating"}>
+			<SelectTrigger>
+				<SelectValue placeholder="Select an interface" />
+			</SelectTrigger>
+			<SelectContent>
+				<SelectGroup>
+					<SelectItem value="floating">
+						Floating captions (default)
+					</SelectItem>
+					<SelectItem value="colour">
+						Static captions coloured by speaker
+					</SelectItem>
+					<SelectItem value="pointer">
+						Static captions with pointers
+					</SelectItem>
+					<SelectItem value="traditional">
+						Traditional static captions
+					</SelectItem>
+				</SelectGroup>
+			</SelectContent>
+		</Select>
 	);
 }
