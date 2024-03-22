afa128c2b07caefb9e0c5165075e60bc546e04c0
nicolelst <nicolelim2608@gmail.com>
2024-03-22 15:23:46 +0800

    add logos to footer

--- application/fe/src/components/layout/footer.tsx
+++ application/fe/src/components/layout/footer.tsx
@@ -1,24 +1,34 @@
 import { Button } from "@/shadcn/components/ui/button";
+import {
+	FileTextIcon,
+	GitHubLogoIcon,
+	InfoCircledIcon,
+} from "@radix-ui/react-icons";
 import { ReactElement } from "react";
 
 export function Footer(): ReactElement {
 	return (
-		<div className="flex flex-row bg-slate-900 text-white items-center sticky bottom-0 py-2 px-8">
+		<div className="flex flex-row bg-slate-900 text-gray-500 items-center sticky bottom-0 py-2 px-8">
 			<div className="flex w-full h-fit">
-				<p className="font-sans text-sm italic">
+				<p className="font-sans text-sm italic text-white">
 					Submitted to the School of Computer Science and Engineering
 					of Nanyang Technological University in 2024
 				</p>
 			</div>
 			{/* TODO add button functionality */}
 			<Button variant="link" className="text-white">
+				<FileTextIcon className="mr-1.5 h-4 w-4" />
 				Report
 			</Button>
+			|
 			<Button variant="link" className="text-white">
+				<GitHubLogoIcon className="mr-1.5 h-4 w-4" />
 				Code
 			</Button>
+			|
 			<Button variant="link" className="text-white">
-				About this FYP
+				<InfoCircledIcon className="mr-1.5 h-4 w-4" />
+				About
 			</Button>
 		</div>
 	);
