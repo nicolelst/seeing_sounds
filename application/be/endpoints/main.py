9e3ee84dd113d8f38845f480abd1d49a4c7e0117
nicolelst <nicolelim2608@gmail.com>
2024-05-01 04:27:41 +0800

    be: save video uploaded to endpoint to local disk

--- application/be/endpoints/main.py
+++ application/be/endpoints/main.py
@@ -1,7 +1,10 @@
-from typing import Union
-
+import os
+import uuid
+import aiofiles
 from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
+
+from video_name_constants import * 
 
 app = FastAPI()
 
@@ -28,4 +31,27 @@ async def upload_video(video: UploadFile, name: str, price: float):
     # https://github.com/tiangolo/fastapi/issues/2257#issuecomment-717522156
     # settings in query params
     # video file in request body 
-    return {"filename": video.filename, "filetype": video.content_type, "settings_test": VideoSettings(name=name, price=price)}
+    request_id = str(uuid.uuid4())
+    request_dir = os.path.join(STORAGE_DIR, request_id)
+
+    if os.path.exists(request_dir): 
+        # if request directory exists, delete contents
+        for existing_file in os.listdir(request_dir):
+            existing_filepath = os.path.join(request_dir, existing_file)
+            if os.path.isfile(existing_filepath):
+                os.remove(existing_filepath)
+    else:
+        # create new request directory 
+        os.makedirs(request_dir, exist_ok=True)
+
+    # TODO check filetype is video
+
+    # save video file to request directory
+    filepath = os.path.join(request_dir, get_input_video_filename(request_id, video.content_type.split("/")[-1]))
+    async with aiofiles.open(filepath, 'wb') as out_file:
+        # use chunks to avoid reading entire video into memory 
+        # https://stackoverflow.com/a/63581187
+        while content := await video.read(1024):  
+            await out_file.write(content)  
+
+    return {"request_id": request_id, "filename": video.filename, "filetype": video.content_type, "settings_test": VideoSettings(name=name, price=price)}
