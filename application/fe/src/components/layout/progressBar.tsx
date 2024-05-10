fb0b78e30fb8867828873bcc8bce2d3356c322c5
nicolelst <nicolelim2608@gmail.com>
2024-05-10 16:57:25 +0800

    handle invalid stage number

--- application/fe/src/components/layout/progressBar.tsx
+++ application/fe/src/components/layout/progressBar.tsx
@@ -3,69 +3,70 @@ import { Progress } from "@/shadcn/components/ui/progress";
 import { ReactElement } from "react";
 
 interface ProgressBarProps {
-	formStageNames: Array<string>;
-	formStageNum: number;
-	setFormStageNum: React.Dispatch<React.SetStateAction<number>>;
+  formStageNames: Array<string>;
+  formStageNum: number;
+  setFormStageNum: React.Dispatch<React.SetStateAction<number>>;
 }
 export function ProgressBar({
-	formStageNames,
-	formStageNum,
-	setFormStageNum,
+  formStageNames,
+  formStageNum,
+  setFormStageNum,
 }: ProgressBarProps): ReactElement {
-	return (
-		<div className="flex flex-col items-center justify-center">
-			<div className="flex flex-row py-2">
-				{formStageNames.map((name, index) => (
-					<div className="flex items-center" key={index}>
-						<StageDisplay
-							stageName={name}
-							stageNum={index + 1}
-							disabled={formStageNum < index + 1}
-							onClick={() => {
-								setFormStageNum(index + 1);
-							}}
-						/>
-						{index < formStageNames.length - 1 ? (
-							<Progress
-								value={formStageNum > index + 1 ? 100 : 0}
-								className="w-32 mx-8"
-							/>
-						) : null}
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  const isValidStageNum = formStageNum <= formStageNames.length;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-row py-2">
+        {formStageNames.map((name, index) => (
+          <div className="flex items-center" key={index}>
+            <StageDisplay
+              stageName={name}
+              stageNum={index + 1}
+              disabled={!isValidStageNum || formStageNum < index + 1}
+              onClick={() => {
+                setFormStageNum(index + 1);
+              }}
+            />
+            {index < formStageNames.length - 1 ? (
+              <Progress
+                value={isValidStageNum && formStageNum > index + 1 ? 100 : 0}
+                className="w-32 mx-8"
+              />
+            ) : null}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 interface StageDisplayProps {
-	stageName: string;
-	stageNum: number;
-	disabled: boolean;
-	onClick: () => void;
+  stageName: string;
+  stageNum: number;
+  disabled: boolean;
+  onClick: () => void;
 }
 
 function StageDisplay({
-	stageName,
-	stageNum,
-	disabled,
-	onClick,
+  stageName,
+  stageNum,
+  disabled,
+  onClick,
 }: StageDisplayProps): ReactElement {
-	return (
-		<div className="flex flex-col justify-center items-center gap-2">
-			<Button
-				className={`rounded-full w-20 h-20 text-3xl ${
-					disabled
-						? "bg-gray-400 hover:bg-gray-400"
-						: "bg-slate-900 hover:bg-slate-900"
-				}`}
-				onClick={onClick}
-				disabled={disabled}
-				type="button"
-			>
-				{stageNum}
-			</Button>
-			<p className="font-semibold">{stageName}</p>
-		</div>
-	);
+  return (
+    <div className="flex flex-col justify-center items-center gap-2">
+      <Button
+        className={`rounded-full w-20 h-20 text-3xl ${
+          disabled
+            ? "bg-gray-400 hover:bg-gray-400"
+            : "bg-slate-900 hover:bg-slate-900"
+        }`}
+        onClick={onClick}
+        disabled={disabled}
+        type="button"
+      >
+        {stageNum}
+      </Button>
+      <p className="font-semibold">{stageName}</p>
+    </div>
+  );
 }
--- application/fe/src/components/stages/invalidStage.tsx
+++ application/fe/src/components/stages/invalidStage.tsx
@@ -1,10 +1,13 @@
+import { FrownIcon } from "lucide-react";
 import { ReactElement } from "react";
 
 export function InvalidStage(): ReactElement {
-	return (
-		<div className="flex w-full h-full items-center justify-center bg-red-300">
-			{/* TODO */}
-			<h1>Something went wrong. Please try again.</h1>
-		</div>
-	);
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col w-full h-full mb-8 items-center justify-center bg-red-300 text-red-600">
+        <FrownIcon className="h-32 w-32 mx-8 mb-8" />
+        <h1 className="text-2xl">Something went wrong. Please try again.</h1>
+      </div>
+    </div>
+  );
 }
